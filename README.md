c9884883c33faf34c3002a14ec0ab73dff67b544
Maikel Chandika <mkdika@gmail.com>
2018-11-07 09:20:22 +0700

    First init, add project structure and supporting files.

---
+++ README.md
@@ -0,0 +1,16 @@
+# Kolak
+
+Kotlin Blog Application, built with [Ktor](https://ktor.io).
+
+## Prerequisite
+
+_Coming soon.._
+
+## Reference
+
+- [Ktor Website](https://ktor.io/quickstart/guides/website.html)
+
+## Copyright and License
+
+Copyright 2018 Maikel Chandika (mkdika@gmail.com). Code released under the 
+MIT License. See [LICENSE](/LICENSE) file.
---
+++ src/Application.kt
@@ -0,0 +1,71 @@
+package com.mkdika
+
+import freemarker.cache.ClassTemplateLoader
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.Compression
+import io.ktor.features.deflate
+import io.ktor.features.gzip
+import io.ktor.features.minimumSize
+import io.ktor.freemarker.FreeMarker
+import io.ktor.freemarker.FreeMarkerContent
+import io.ktor.http.ContentType
+import io.ktor.http.content.resources
+import io.ktor.http.content.static
+import io.ktor.response.respond
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.sessions.Sessions
+import io.ktor.sessions.cookie
+import kotlin.collections.List
+import kotlin.collections.listOf
+import kotlin.collections.mapOf
+import kotlin.collections.set
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(FreeMarker) {
+        templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
+    }
+
+    install(Sessions) {
+        cookie<MySession>("MY_SESSION") {
+            cookie.extensions["SameSite"] = "lax"
+        }
+    }
+
+    install(Compression) {
+        gzip {
+            priority = 1.0
+        }
+        deflate {
+            priority = 10.0
+            minimumSize(1024) // condition
+        }
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        get("/html-freemarker") {
+            call.respond(FreeMarkerContent("index.ftl", mapOf("data" to IndexData(listOf(1, 2, 3))), ""))
+        }
+
+        // Static feature. Try to access `/static/ktor_logo.svg`
+        static("/static") {
+            resources("static")
+        }
+    }
+}
+
+data class IndexData(val items: List<Int>)
+
+data class MySession(val count: Int = 0)
+
